6a24db3fcadcd6fdff6eb73a02b70ab0b12595db
RajeshVSakthivel1 <rajesh.venugopalsakthivel@nationwide.co.uk>
2023-05-14 00:32:09 +0100

    development

--- examples/profile/controls/kubernetespods.rb
+++ examples/profile/controls/kubernetespods.rb
@@ -1,30 +1,20 @@
 title "Kubernetes Basics"
 
-control "kubernetes_pod" do
+control "k8s-1.0" do
   impact 1.0
   title "Validate built-in namespaces"
   desc "The kube-system, kube-public, and default namespaces should exist"
 
-#   describe kubernetes_pod(api: 'v1', type: 'namespaces', name: 'kube-system') do
-#     it { should exist }
-#   end
-#   describe k8sobject(api: 'v1', type: 'namespaces', name: 'banking-cco-dev1') do
-#     it { should exist }
-#   end
-#   describe k8sobject(api: 'v1', type: 'namespaces', name: 'banking-aet-dev1') do
-#     it { should exist }
-#   end
-#   describe k8sobject(api: 'v1', type: 'namespaces', name: 'banking-lao-dev1') do
-#     it { should exist }
-#   end
-
-describe kubernetes_pod(namespace: 'banking-cco-dev1') do
+  describe kubernetes_pod(api: 'v1', type: 'namespaces', name: 'kube-system') do
     it { should exist }
-    it { should be_running }
+  end
+  describe k8sobject(api: 'v1', type: 'namespaces', name: 'banking-cco-dev1') do
+    it { should exist }
+  end
+  describe k8sobject(api: 'v1', type: 'namespaces', name: 'banking-aet-dev1') do
+    it { should exist }
+  end
+  describe k8sobject(api: 'v1', type: 'namespaces', name: 'banking-lao-dev1') do
+    it { should exist }
   end
 end
-
-describe kubernetes_pod(name: 'eo-svc-lao-95b498d95-mftjn', namespace: 'banking-lao-dev1') do
-    it { should exist }
-    it { should be_running }
-  end
