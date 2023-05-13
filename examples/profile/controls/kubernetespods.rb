2e12ba1015e723f5dbe2548b92b9db2c4c8099bf
RajeshVSakthivel1 <rajesh.venugopalsakthivel@nationwide.co.uk>
2023-05-13 23:57:12 +0100

    development

--- examples/profile/controls/kubernetespods.rb
+++ examples/profile/controls/kubernetespods.rb
@@ -1,6 +1,6 @@
 title "Kubernetes Basics"
 
-control "k8s-1.0" do
+control "kubernetes_pod" do
   impact 1.0
   title "Validate built-in namespaces"
   desc "The kube-system, kube-public, and default namespaces should exist"
